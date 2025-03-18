d1d58f0ee09888d745bbeeba8ac0ee720f775c09
Alonzo Velez <alonzovelez9@gmail.com>
2025-03-18 16:20:30 -0400

    Finished Step 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,32 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
+const API_KEY = import.meta.env.VITE_APP_API_KEY;
 
 function App() {
-  return <></>;
+  const [list, setList] = useState(null);
+
+  const fetchAllCoinData = async () => {
+    const response = await fetch(
+      "https://min-api.cryptocompare.com/data/all/coinlist?&api_key" + API_KEY
+    );
+    const json = await response.json();
+    setList(json);
+  };
+
+  useEffect(() => {});
+
+  return (
+    <div className="whole-page">
+      <h1>My Crypto List</h1>
+      <button onClick={fetchAllCoinData}></button>
+      <ul>
+        {list &&
+          Object.entries(list.Data).map(([coin]) => (
+            <li key={list.Data[coin].FullName}>{list.Data[coin].FullName}</li>
+          ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
